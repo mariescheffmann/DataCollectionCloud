786ea62104e1566dc35d79019ac1a88be1f30f75
mariescheffmann <mariemadsen26@yahoo.dk>
2025-11-18 09:28:35 +0100

    updated web to use same network as other containers

--- Web/src/server.ts
+++ Web/src/server.ts
@@ -25,6 +25,6 @@ app.get('/', (req, res) => {
   res.send(html);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
