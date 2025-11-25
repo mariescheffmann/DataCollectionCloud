07d6ce4e3808aa507343d020d9b825c927d394b2
Louip22 <lrusanderp@gmail.com>
2025-11-25 15:27:32 +0100

    start of orm-setup (not functional)

---
+++ Web/src/entities/Data.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+
+@Entity()
+export class Data {
+    @PrimaryKey()
+    id!: number;
+
+    @Property()
+    topic!: string;
+
+    @Property()
+    payload!: string;
+}
