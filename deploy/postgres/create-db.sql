7bc86243fc311dafc70ed16857ead29f2554ffac
Louip22 <lrusanderp@gmail.com>
2025-11-25 15:14:23 +0100

    add start- and end-time on recipes

--- deploy/postgres/create-db.sql
+++ deploy/postgres/create-db.sql
@@ -80,7 +80,7 @@ CREATE TABLE recipes (
     g_number VARCHAR(355),
     material_id VARCHAR(355),
     stage VARCHAR(355),
-    recipe_version VARCHAR(355)
+    recipe_version VARCHAR(355),
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP
 );
-
--- TODO: insert recipies???? -lrp
